5a6cef7006403405e15868bcfb40362034f959b8
Roslan Saidi <roslansaidi@ymail.com>
2021-07-08 11:36:28 +0800

    TNB Training

---
+++ resources/js/components/HideAndShowComponent.vue
@@ -0,0 +1,29 @@
+<template>
+	<span>
+		<div v-if="is_show">
+			Show Me - I'm Here
+		</div>
+		<div>
+			<button @click="showUp">Show</button>
+			<button @click="hideUp">Hide</button>
+		</div>
+	</span>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				is_show: false,
+			}
+		},
+		methods: {
+			showUp() {
+				this.is_show = true
+			},
+
+			hideUp() {
+				this.is_show = false
+			},
+		},
+	}
+</script>
---
+++ resources/js/components/RoslanComponent.vue
@@ -0,0 +1,29 @@
+<template>
+	<span>
+		<div>
+			Butang yang telah di tekan: {{ number }}
+		</div>
+		<div>
+			<button @click="countUp">Click Me</button>
+		</div>
+	</span>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				number: 0,
+			}
+		},
+		methods: {
+			countUp() {
+				this.number++
+			},
+		},
+	}
+</script>
+
+<style>
+	
+</style>
